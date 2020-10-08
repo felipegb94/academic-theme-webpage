d09ca24a25f3b66da640a6cf39bad7bd06bb8a69
Felipe Gutierrez-Barragan <fgutierrez3@wisc.edu>
2020-10-07 18:57:16 -0700

    changed sync absolute path and updated resume

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@
 USER=felipe
 HOST=best-linux.cs.wisc.edu
 REMOTE_PATH=/u/f/e/felipe/public/html/
-LOCAL_PATH=$HOME/Dropbox/Webpage/academic-theme-webpage/public/
+LOCAL_PATH=$HOME/Dropbox/webpages/academic-theme-webpage/public/
 
 # rsync --rsh='ssh -p 22222' -vv --checksum --recursive --update --keep-dirlinks --stats --human-readable --progress --itemize-changes ${LOCAL_PATH} ${USER}@${HOST}:${REMOTE_PATH}
 rsync -vv --checksum --recursive --update --keep-dirlinks \
